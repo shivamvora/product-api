f8b0b7547ba6d706a581767c3730715100bde6ae
shivam vora <vorashivam0@gmail.com>
2022-07-20 22:54:12 +0530

    sorting paging

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,12 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort("-name price");
+  const products = await Product.find({}).select("name price");
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, name, price, company, sort } = req.query;
+  const { featured, name, price, company, sort, fields } = req.query;
   const queryObj = {};
   if (featured) {
     queryObj.featured = featured;
@@ -27,7 +27,24 @@ const getAllProducts = async (req, res) => {
   if (sort) {
     const sortList = sort.split(",").join(" ");
     result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
   }
+
+  if (fields) {
+    console.log("fields: ", fields);
+    const fieldsList = fields.split(",").join(" ");
+    console.log("fieldsList: ", fieldsList);
+    result = result.select(fieldsList);
+  }
+
+  // pagination
+
+  const page = Number(req.query.page) || 1;
+  const pageLimit = Number(req.query.page) || 10;
+  const skip = (page - 1) * pageLimit;
+  result = result.limit(pageLimit).skip(skip);
+
   const products = await result;
   res.status(200).json({ products, length: products.length });
 };
